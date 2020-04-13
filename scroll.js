db5aa51d83bf0a70d2bed7d71afd5c2c5db267f3
EstudiLaris <62201268+EstudiLaris@users.noreply.github.com>
2020-04-13 17:04:52 +0200

    acabat1

--- scroll.js
+++ scroll.js
@@ -4,16 +4,20 @@ window.onscroll = function() {
 
 function menuScroll() {
     var topBar = document.getElementById("topBar");
+    var _header = document.getElementById("header");
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         document.getElementById("subTitol").style.display = "none";
         document.getElementById("titol").style.borderBottom = "none";
         topBar.classList.add('animateTop');
         topBar.style.position = "fixed";
-        /*topBar.style.top = "0";*/
+        /* _header.classList.add('animateTop');
+        _header.style.position = "fixed";
+        _header.style.top = 0; */
     } else {
         document.getElementById("subTitol").style.display = "block";
         topBar.classList.remove('animateTop');
         document.getElementById("titol").style.borderBottom = "black solid 3px";
         topBar.style.position = "relative";
+        /* _header.style.position = "relative"; */
     }
 }
